8a4f14f9b912cf32ebfae6d91d542aa47a8808ce
Ben-Mostyn <ben_mostyn@live.co.uk>
2022-10-10 19:01:19 +0100

    Working code

--- public/app.js
+++ public/app.js
@@ -1,12 +1,3 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 import { ResultsList } from "./Classes/ResultsList.js";
 import { UserResults } from "./Classes/User_Results.js";
 import { countries } from "./CountryArray.js";
@@ -18,31 +9,21 @@ const list = new ResultsList(ul);
 const submitBtn = document.getElementById("generateBtn");
 let arrEl = [];
 randCountry = Math.floor(Math.random() * countries.length);
-arrEl = countries.splice(randCountry, 1);
-let country = arrEl.toString();
-const flagApi = () => __awaiter(void 0, void 0, void 0, function* () {
-    let res = yield fetch("https://cdn.jsdelivr.net/npm/country-flag-emoji-json@2.0.0/dist/by-code.json");
-    console.log(res);
-    let data = yield res.json();
-    console.log(data);
-    Object.values(data).forEach((value) => {
-        console.log(value.emoji);
-    });
-});
 const submitHandle = (e) => {
     if (countries.length >= 1 && input.value) {
+        arrEl = countries.splice(randCountry, 1);
+        let country = arrEl.toString();
         console.log(country, input.value);
+        console.log(countries);
         const user = new UserResults(input.value, country);
         form.reset();
         list.render(user);
     }
-    else {
-        submitBtn.disabled = true;
-        console.log("out of countries");
+    if (!input.value) {
+        console.log("Type something");
     }
 };
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     submitHandle(e);
-    flagApi();
 });
--- src/app.ts
+++ src/app.ts
@@ -10,39 +10,41 @@ const list = new ResultsList(ul);
 const submitBtn = document.getElementById("generateBtn") as HTMLButtonElement;
 let arrEl: string[] = [];
 randCountry = Math.floor(Math.random() * countries.length);
-arrEl = countries.splice(randCountry, 1);
-let country = arrEl.toString();
 
 //! API CALL
 
-const flagApi = async () => {
-  // let res = await fetch("https://rickandmortyapi.com/api/character/");
-  let res = await fetch(
-    "https://cdn.jsdelivr.net/npm/country-flag-emoji-json@2.0.0/dist/by-code.json"
-  );
-  console.log(res);
-  let data = await res.json();
-  console.log(data);
+// const flagApi = async () => {
+//   // let res = await fetch("https://rickandmortyapi.com/api/character/");
+//   let res = await fetch(
+//     "https://cdn.jsdelivr.net/npm/country-flag-emoji-json@2.0.0/dist/by-code.json"
+//   );
+//   console.log(res);
+//   let data = await res.json();
+//   console.log(data);
 
-  Object.values(data).forEach((value: object) => {
-    console.log(value.emoji);
-  });
-};
+//   Object.values(data).forEach((value: object) => {
+//     console.log(value.emoji);
+//   });
+// };
 
 const submitHandle = (e: Event): void => {
   if (countries.length >= 1 && input.value) {
+    arrEl = countries.splice(randCountry, 1);
+    let country = arrEl.toString();
     console.log(country, input.value);
+    console.log(countries);
     const user = new UserResults(input.value, country);
     form.reset();
     list.render(user);
-  } else {
-    submitBtn.disabled = true;
-    console.log("out of countries");
+  }
+  if (!input.value) {
+    // submitBtn.disabled = true;
+    console.log("Type something");
   }
 };
 //! Button
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
   submitHandle(e);
-  flagApi();
+  // flagApi();
 });
